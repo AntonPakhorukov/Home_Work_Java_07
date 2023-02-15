2f8f2526c300b586ed9b8dd0287b0ac474a3a05f
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-02-15 20:59:34 +0700

    OOP task1

---
+++ Fruits.java
@@ -0,0 +1,10 @@
+/**
+ * Создать класс, в котором будет храниться следующая информация
+ * name, color, weight, price
+ */
+public class Fruits {
+    String name;
+    String color;
+    double weight;
+    double price;    
+}
